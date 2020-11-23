78d61f7df5669d4cd7a8cadedf8b230710efef5c
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2020-11-23 18:11:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var familyarry=["https://tse3.mm.bing.net/th?id=OIP.AlTlmKGOra4GFz_697wx1wHaS1&pid=Api&P=0&w=300&h=300",
+"https://webstockreview.net/images/father-clipart-10.jpg",
+ "https://tse3.explicit.bing.net/th?id=OIP.ScaGOXitPka4DFmxgzwyiwHaF_&pid=Api&P=0&w=198&h=161",
+"https://www.graphicsfactory.com/clip-art/image_files/image/3/1681723-cartoon-African-American-boy-holding-arm-up-vector-clipart.jpg",
+"https://png.pngtree.com/png-clipart/20190611/original/pngtree-boy-endorsement-package-to-go-to-school-png-image_2757038.jpg"];
+var namesarry=["Anubha Jian","Akshay jain","Bhavya jain ","Devaansh jain","Arham jain "];
+var i=0;
+function a(){
+document.getElementById("album").src=familyarry[i];
+document.getElementById("name").innerHTML=namesarry[i];
+i++;
+}
