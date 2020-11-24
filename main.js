47f7da62604e4983b8ca1dacefe5447778b62a2c
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2020-11-24 13:49:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ var familyarry=["https://tse3.mm.bing.net/th?id=OIP.AlTlmKGOra4GFz_697wx1wHaS1&p
 "https://www.graphicsfactory.com/clip-art/image_files/image/3/1681723-cartoon-African-American-boy-holding-arm-up-vector-clipart.jpg",
 "https://png.pngtree.com/png-clipart/20190611/original/pngtree-boy-endorsement-package-to-go-to-school-png-image_2757038.jpg"];
 var namesarry=["Anubha Jian","Akshay jain","Bhavya jain ","Devaansh jain","Arham jain "];
-var i=0;
-function a(){
-document.getElementById("album").src=familyarry[i];
-document.getElementById("name").innerHTML=namesarry[i];
-i++;
-}
+
+var i = 0; function nextpage() { if(i == 5)
+    {
+        i=0;
+    }
+        document.getElementById("album").src = familyarry[i]; i++;
+document.getElementById("name").innerHTML=namesarry;
+    }
